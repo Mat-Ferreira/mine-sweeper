ba5b5e1e67d4d28fc7f195a6994221c21a23cce8
ma_fe <ma_fe@DESKTOP-MDI66QI>
2022-12-14 21:20:57 -0300

    Add board setting methods

--- Mine-Sweep/src/br/com/maf/ms/logic/Board.java
+++ Mine-Sweep/src/br/com/maf/ms/logic/Board.java
@@ -1,5 +1,62 @@
 package br.com.maf.ms.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Board {
+	private int lines;
+	private int columns;
 	
+	private int amountOfMines;
+	
+	private final List<Camp> camps = new ArrayList<>();
+
+	public Board(int lines, int columns, int amountOfMines) {
+		this.lines = lines;
+		this.columns = columns;
+		this.amountOfMines = amountOfMines;
+		
+		creatCamps();
+		setNeighbours();
+		setMineCamps();
+		
+	}
+
+	private void creatCamps() {
+		for (int l = 0; l < lines; l++) {
+			for(int c = 0; c < columns; c++) {
+				camps.add(new Camp(l,c));
+			}
+		}	
+	}
+	
+	private void setNeighbours() {
+		for (Camp c1: camps) {
+			for(Camp c2: camps) {
+				c1.addNeighbour(c2);
+			}
+		}
+	}
+
+	private void setMineCamps() {
+		 int minesSet = 0;
+		 Predicate<Camp> mined = c -> c.isMineField();
+		 
+		 do { 
+			minesSet = (int) camps.stream().filter(mined).count();
+			int random = (int) (Math.random() * camps.size());
+			camps.get(random).setMine();	
+		} 
+		while (minesSet < amountOfMines);
+	}
+	
+	public boolean reachObjective() {
+		return camps.stream().allMatch(c -> c.reachObjective());
+	}
+	
+	public void restartBoard() {
+		camps.stream().forEach(c -> c.restart());
+		setMineCamps();
+	}
 }
